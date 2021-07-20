717ddef58d2b53c48ed3ec286ea3f308209e7918
nedurland <63692293+nedurland@users.noreply.github.com>
2021-07-19 22:10:25 -0700

    Update defs.h

--- defs.h
+++ defs.h
@@ -120,6 +120,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int             modEx(int); // for lab 1
 
 // swtch.S
 void            swtch(struct context**, struct context*);
